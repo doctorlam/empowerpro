9d2126cea240b4e47df3469f98d4bbbffa014f5c
Chris <chris.lam.unt@gmail.com>
2017-11-29 09:59:31 -0600

    deleted some old migration files for heroku

--- db/migrate/20171117002419_drop_table.rb
+++
@@ -1,9 +0,0 @@
-class DropTable < ActiveRecord::Migration[5.1]
- def up
-    drop_table :assignments
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20171117002605_drop_roles_table.rb
+++
@@ -1,9 +0,0 @@
-class DropRolesTable < ActiveRecord::Migration[5.1]
-   def up
-    drop_table :roles
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
